2a0a84678f8de6490b3585ca3557809fb3c09d38
Andante23 <98266983+Andante23@users.noreply.github.com>
2023-02-28 17:50:50 +0900

    Update string.js

--- javascript/datatype/string/string.js
+++ javascript/datatype/string/string.js
@@ -1,27 +1,48 @@
 
 
-let str = 'back';  // 작은 따음표
+let text3 = 'a'; // 작은 따음표
 
-let str1 = "tick";  // 큰 따음표
+let text4 = "a"; // 큰 따음표
 
-let str2 = `icon`;  //  백틱 (ECMA6부터 추가되었다. 템퍼럴 리터럴이다.)
+let text5 = `2`; // 백틱
 
 
 
 
 // 문자열 합치기
 
-// 따음표를 이용한 합치기
+// + 연산자를 이용한 문자열 합치기
 
-let resultstring = 'an' + 'dan' + 'te';  // andante
+let resultstring1 = 'an' + 'dan' + 'te';
+let resultstring2 = "an" + "dan" + "te";
+let resultstring3 = `an` + `dan` + `te`;
+console.log(resultstring1);
+console.log(resultstring2);
+console.log(resultstring3);
+
+
+
 
 
 // 변수를 이용한 문자열 합치기
 
+let str = "back";
+let str1 = "tick";
+
+let str2 = 'ki';
+let str3 = 'ss';
+
+let str4 = `wo`;
+let str5 = `od`;
+
 let result = str + str1;
+let result2 = str2 + str3;
+let result3 = str4 + str5;
 
-// 표현식 ${} 를 이용한 문자열 합치기 (ECMA6부터 추가된 기능)
 
-// let result = ${str} + ${str1};  backtick
+console.log(result); // backtick
+console.log(result2);  // kiss
+console.log(result3);  // wood
+
 
 
