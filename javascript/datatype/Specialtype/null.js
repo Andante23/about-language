e4f23b757d50b7d7be7fd109bc7178f430ca40b9
Andante23 <98266983+Andante23@users.noreply.github.com>
2023-03-04 12:52:53 +0900

    Delete javascript directory

--- javascript/datatype/Specialtype/null.js
+++
@@ -1,6 +0,0 @@
-
-//  null : 변수에 값이 없음을 의도적으로 알리고 싶을 때 사용한다.
-
-const amifat = null;
-console.log(amifat); // null
-
--- javascript/datatype/Specialtype/undefined.js
+++
@@ -1,7 +0,0 @@
-
-
-// undefined : 변수에 값을  지정하지 않았을때 나타난다.
-
-let something;
-
-console.log(something);  // undefined
