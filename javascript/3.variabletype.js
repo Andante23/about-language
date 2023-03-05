aed57e84bae427d9ad9e1e584f88d61eba47508b
Andante23 <98266983+Andante23@users.noreply.github.com>
2023-03-05 15:17:53 +0900

    Add files via upload

---
+++ javascript/3.variabletype.js
@@ -0,0 +1,24 @@
+
+
+
+var arr = [100, 200, 300];
+console.log(typeof (arr));
+
+
+
+
+var text;
+var text2 = 'string';
+var num = 1234;
+
+
+
+console.log(typeof (text));
+console.log(typeof (text2));
+console.log(typeof (num));
+
+
+
+
+
+
---
+++ javascript/4.variabletype2.js
@@ -0,0 +1,24 @@
+
+
+// 다음 변수 a를 typeof (a)로 넣었을 때 출력될 값과의 연결이 알맞지 않은 것은 ?
+
+
+var a = 1;
+
+typeof (a); // number
+
+
+var a = 2.22;
+
+typeof (a); // number
+
+
+var a = 'p';
+
+typeof (a); // string
+
+
+var a = [1, 2, 3];
+typeof (a); // object 
+
+
